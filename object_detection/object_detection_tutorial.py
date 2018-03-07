c31909952b0893d144fcb7b1b008e209f0f5fefc
atopheim <torbjornop@outlook.com>
2018-03-07 17:21:53 +0100

    [added picture]

--- object_detection/object_detection_tutorial.py
+++ object_detection/object_detection_tutorial.py
@@ -29,7 +29,6 @@ if tf.__version__ < '1.4.0':
 
 # ## Env setup
 
-# In[26]:
 
 
 # This is needed to display the images.
@@ -39,7 +38,6 @@ if tf.__version__ < '1.4.0':
 # ## Object detection imports
 # Here are the imports from the object detection module.
 
-# In[27]:
 
 
 from utils import label_map_util
@@ -55,9 +53,6 @@ from utils import visualization_utils as vis_util
 # 
 # By default we use an "SSD with Mobilenet" model here. See the [detection model zoo](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
 
-# In[28]:
-
-
 # What model to download.
 MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
 MODEL_FILE = MODEL_NAME + '.tar.gz'
@@ -71,7 +66,7 @@ PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
 
 NUM_CLASSES = 90
 
-
+##############################################################3
 # ## Download Model
 #opener = urllib.request.URLopener()
 #opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
@@ -81,6 +76,7 @@ NUM_CLASSES = 90
 #  if 'frozen_inference_graph.pb' in file_name:
 #    tar_file.extract(file, os.getcwd())
 
+##########################################################3333
 
 # ## Load a (frozen) Tensorflow model into memory.
 detection_graph = tf.Graph()
@@ -91,49 +87,34 @@ with detection_graph.as_default():
     od_graph_def.ParseFromString(serialized_graph)
     tf.import_graph_def(od_graph_def, name='')
 
-
 # ## Loading label map
-# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
-
-# In[31]:
-
+# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine.
 
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-
 # ## Helper code
-
-# In[32]:
-
 
 def load_image_into_numpy_array(image):
   (im_width, im_height) = image.size
   return np.array(image.getdata()).reshape(
       (im_height, im_width, 3)).astype(np.uint8)
 
-
 # # Detection
-
-# In[33]:
-
 
 # For the sake of simplicity we will use only 2 images:
 # image1.jpg
 # image2.jpg
 # If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
 PATH_TO_TEST_IMAGES_DIR = 'test_images'
-TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
+TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1,4) ]
 #PATH_TO_TEST_IMAGES_DIR = '/home/torbjoern/'
 #TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'irises.jpg')]
 print(TEST_IMAGE_PATHS)
 
 # Size, in pixels, of the output images.
-IMAGE_SIZE = (1024, 636)
-
-
-# In[40]:
+IMAGE_SIZE = (12, 8)
 
 
 def run_inference_for_single_image(image, graph):
@@ -182,12 +163,19 @@ def run_inference_for_single_image(image, graph):
         output_dict['detection_masks'] = output_dict['detection_masks'][0]  
   return output_dict
 
+####################################
+#       Configure here             #
+####################################
 
-# In[35]:
+threshold = 0.8
+max_objects = 10
+valid_boxes = []
+
 
 
 for image_path in TEST_IMAGE_PATHS:
   image = Image.open(image_path)
+  (im_width, im_height) = image.size
   # the array based representation of the image will be used later in order to prepare the
   # result image with boxes and labels on it.
   image_np = load_image_into_numpy_array(image)
@@ -196,7 +184,7 @@ for image_path in TEST_IMAGE_PATHS:
   # Actual detection.
   output_dict = run_inference_for_single_image(image_np, detection_graph)
   # Visualization of the results of a detection.
-  print(output_dict)
+  # print(output_dict)
   vis_util.visualize_boxes_and_labels_on_image_array(
       image_np,
       output_dict['detection_boxes'],
@@ -206,34 +194,21 @@ for image_path in TEST_IMAGE_PATHS:
       instance_masks=output_dict.get('detection_masks'),
       use_normalized_coordinates=True,
       line_thickness=8)
-  plt.figure(figsize=IMAGE_SIZE)
-  plt.imshow(image_np)
 
-####################################
-#	Configure here		   #
-####################################
+  for i in range(max_objects):
+    if output_dict['detection_scores'][i] > threshold:
+      valid_boxes.append(output_dict['detection_boxes'][i])
 
-threshold = 0.8
-max_objects = 10
-valid_boxes = []
-picture_size_x = 1024
-picture_size_y = 636
-
-
-####################################
-
-for i in range(max_objects):
-  if output_dict['detection_scores'][i] > threshold:
-    valid_boxes.append(output_dict['detection_boxes'][i])
-
-midpoints = []
-num_boxes = len(valid_boxes)
-for i in range(num_boxes):
-  x = valid_boxes[i][3] - valid_boxes[i][1]
-  y = valid_boxes[i][2] - valid_boxes[i][0]
-  midpoints.append([(x/2)*picture_size_x,(y/2)*picture_size_y])
-for element in midpoints:
+  midpoints = []
+  num_boxes = len(valid_boxes)
+  for i in range(num_boxes):
+    x = valid_boxes[i][3] - valid_boxes[i][1]
+    y = valid_boxes[i][2] - valid_boxes[i][0]
+    midpoints.append([(x/2)*im_width,(y/2)*im_height])
   i = 1
-  print("Object",i," -->  X position ",element[0],"Y position: ",element[1])
-  i += 1
+  for element in midpoints:
+    print("Object",i," -->  X position ",element[0],"Y position: ",element[1])
+    i += 1
 
+  #plt.figure(figsize=IMAGE_SIZE)
+  #plt.imshow(image_np)
